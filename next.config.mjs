c5c8783395084d9e4042455b32cd05853acb1d50
Amaury <iamaury1380@gmail.com>
2024-10-20 23:23:55 +0200

    Fix attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
-  }
-};
-
-export default withNextIntl(nextConfig);
+export default withNextIntl();
