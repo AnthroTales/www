fa2ac1e7a0249781e11bcb653133f5679eb7ccfa
Amaury <iamaury1380@gmail.com>
2024-10-20 23:50:36 +0200

    New version

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -10,15 +9,4 @@ const nextConfig = {
   },
 };
 
-const wrappedConfig = (config) => {
-  try {
-    return withNextIntl(config);
-  } catch (error) {
-    if (!(error instanceof Error)) {
-      throw new Error("An error occurred: " + error);
-    }
-    throw error;
-  }
-};
-
-export default wrappedConfig(nextConfig);
+export default withNextIntl(nextConfig);
